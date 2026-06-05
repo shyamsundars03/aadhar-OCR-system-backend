38bb476c83308c7ef1201e6c49ffaff9a6e5f9de
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-06-05 23:23:29 +0530

    fix: allow all CORS origins to resolve Vercel deployment block

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,23 +1,7 @@
 import cors from 'cors';
 
-const allowedOrigins = [
-  'http://localhost:5173', 
-  'http://127.0.0.1:5173',
-  'http://localhost:5174', 
-  'http://127.0.0.1:5174',
-  process.env.FRONTEND_URL
-].filter(Boolean);
-
 const corsOptions = {
-  origin: (origin, callback) => {
-   
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: '*',
   optionsSuccessStatus: 200
 };
 
